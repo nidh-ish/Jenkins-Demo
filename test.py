c49f29e03b7418ee13421ee724c0c09638d7fcd8
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-11-23 01:14:28 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import unittest
+from build import *
+
+class SampleTest(unittest.TestCase):
+    def test_addition(self):
+        a = 7
+        b = 3
+        expected = 10
+        self.assertEqual(addition(a, b), expected)
+    def test_subtraction(self):
+        a = 10
+        b = 2
+        expected = 8
+        self.assertEqual(subtraction(a, b), expected)
+    def test_multiplication(self):
+        a = 10
+        b = 12
+        expected = 120
+        self.assertEqual(multiplication(a, b), expected)
+    def test_division(self):
+        a = 14
+        b = 2
+        expected = 7
+        self.assertEqual(division(a, b), expected)
+    def test_mean(self):
+        l = [1, 2, 3, 4, 5]
+        expected = 3
+        self.assertEqual(mean(l), expected)
+
+if __name__ == '__main__':
+    unittest.main()
